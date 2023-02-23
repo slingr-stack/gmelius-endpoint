3392fc9755ab36c781e00768a3ee7ba09477073a
Alejandro Greggio <agreggio@slingr.io>
2023-02-23 17:47:34 -0300

    #13417 Standardize gmelius endpoint

---
+++ flowSteps/sequencesEnroll/step.js
@@ -0,0 +1,31 @@
+/**
+ * This flow step will send generic request.
+ *
+ * @param {object} inputs
+ */
+step.sequencesEnroll = function (inputs) {
+
+    var inputs = {
+        sequenceId: inputs.contactFirstName || "",
+        contactEmail: inputs.contactEmail || "",
+        contactFirstName: inputs.contactFirstName || ""
+    };
+
+    var body = {
+        "email_address": inputs.contactEmail,
+        "variables": [
+            {
+                "name": "to.fname",
+                "value": inputs.contactFirstName,
+            }
+        ]
+    };
+
+    var options = {
+        path: '/auth/sequences/enroll/' + inputs.sequenceId,
+        body: body
+    };
+
+    return endpoint._post(options);
+
+}
